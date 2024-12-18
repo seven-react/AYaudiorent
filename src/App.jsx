878abca28706d6907b9e08193ce4267fbd0ebfba
seven-react <yosefbaye777@gmail.com>
2024-12-18 16:22:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-500 font-bold text-[2em]'>Welcome to AYaudiorent</h1>
+      
+    </div>
+  );
+}
+
+export default App;
